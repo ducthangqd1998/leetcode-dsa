0b0dd56c42278988f97f721d93cbd4761c29e951
ducthangqd1998 <maiducthang.it@gmail.com>
2024-04-24 07:52:34 +0700

    Add 155. Min Stack

---
+++ stack/155-min-stack.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+using namespace std;
+
+
+class Node {
+public:
+    int num;
+    Node* node; 
+
+    Node (int n) {
+        this->num = n;
+        this->node = NULL;
+    }
+
+};
+
+class MinStack {
+public:
+    Node* root;
+    MinStack() {
+        root = NULL;
+    }
+    
+    
+    void push(int val) {
+        Node* temp = new Node(val);
+        temp->num = val;
+        temp->node = this->root;
+        this->root = temp;
+    }
+    
+    void pop() {
+        Node* temp = this->root->node;
+        root = temp;
+    }
+    
+    int top() {
+        return this->root->num;
+    }
+    
+    int getMin() {
+        Node* current = this->root;
+        int min = current->num;
+        while (current != NULL) {
+            if(current->num < min) min = current->num;
+            current = current->node;
+        }
+        return min;
+    }
+};
+
+
+int main() {
+    MinStack minStack;
+    minStack.push(-2);
+    minStack.push(0);
+    minStack.push(-3);
+    minStack.getMin(); // return -3
+    minStack.pop();
+    minStack.top();    // return 0
+    minStack.getMin();
+    return 0;
+}
