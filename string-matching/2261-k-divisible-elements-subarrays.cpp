28541a1746ddd526c319f8f9487c52b6f47d74ee
ducthangqd1998 <maiducthang.it@gmail.com>
2024-04-28 15:18:35 +0700

    Add 2261. K Divisible Elements Subarrays

---
+++ string-matching/2261-k-divisible-elements-subarrays.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <stdio.h>
+#include <map>
+#include <set>
+
+using namespace std;
+
+long long mod = 1e11 + 7;
+
+class Solution
+{
+public:
+    int countDistinct(vector<int> &nums, int k, int p)
+    {
+        set<long long> c;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            long long hash = 0;
+            int check = 0;
+            for (int j = i; j < nums.size(); j++)
+            {
+                if (!(nums[j] % p))
+                    check += 1;
+                if (check > k)
+                    break;
+                hash = (hash * 201 + nums[j]) % mod;
+                c.insert(hash);
+            }
+        }
+        return c.size();
+    }
+};
+
+int main()
+{
+    Solution solution;
+    // vector<int> n = {14,11,6,9,3,15};
+    // solution.countDistinct(n, 4, 7);
+    // vector<int> n = {13,4,14,13,15,4,8,13,4,12};
+    // solution.countDistinct(n, 5, 14);
+    // vector<int> n = {10,2,17,7,20};
+    // solution.countDistinct(n, 1, 10);
+    vector<int> n = {2, 3, 3, 2, 2};
+    solution.countDistinct(n, 2, 2);
+    return 1;
+}
