76b103b349f1b9a234659e05bf59da857cfa105a
ducthangqd1998 <maiducthang.it@gmail.com>
2024-06-27 21:06:48 +0700

    746. Min Cost Climbing Stairs

---
+++ dp/746-min-cost-climbing-stairs.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        int prev1 = 0, prev2 = 0, current = 0;
+
+        for (int i = 2; i <= n; i++) {
+            current = min(prev1 + cost[i - 1], prev2 + cost[i - 2]);
+            prev2 = prev1;
+            prev1 = current;
+        }
+
+        return current;
+    }
+};
